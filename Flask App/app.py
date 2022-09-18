7bccb49d7f80af31d11deaa3e66404cd85af05a0
rprkh <r.parikh@somaiya.edu>
2022-09-18 17:21:34 +0530

    include functionality for song uploads

--- Flask App/app.py
+++ Flask App/app.py
@@ -1,30 +1,31 @@
-from flask import Flask, render_template, request
 import os
-import pickle
 import lightgbm
-import librosa
-
-UPLOAD_FOLDER = 'upload_folder'
-ALLOWED_EXTENSIONS = {'wav'}
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-model = pickle.load(open('assets/opt_lgbm_model.pkl', 'rb'))
+app.config['UPLOAD_FOLDER'] = 'uploads_folder/'
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        for files in os.listdir(app.config['UPLOAD_FOLDER']):
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], files))
 
-@app.route('/predict', methods = ['POST', 'GET'])
-def predict():
-    audio_file = request.form['audio_file']
-    result = ''
+        file = request.files['file']
+        filename = secure_filename(file.filename)
 
-    if(audio_file.endswith('.wav') == False):
-        error_message = 'Upload only a .wav file'
-        result = 'Cannot predict genre due to incorrect file upload'
+        if(filename.endswith('.wav') == True):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            os.rename(os.path.join(app.config['UPLOAD_FOLDER'], filename),
+                    os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded_audio_file.wav'))
+            message = 'File upload successful'
+        else:
+            message = 'Upload only .wav files'
 
     return render_template('index.html', **locals())
 
